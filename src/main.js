61e2be78e1b1d0c5d942f7994342a53d545ebdf9
nurbekdayo <yourmotherknowsbest022@gmail.com>
2024-02-22 22:38:41 +0800

    added another section

--- src/main.js
+++ src/main.js
@@ -4,6 +4,8 @@ import '../styles/header.css'
 import '../styles/main-sect.css'
 import'../styles/footer.css'
 import'../styles/about.css'
+import '../styles/skills.css'
+import '../styles/project.css'
 
 import lazyLoad from './lazyLoad'
 import fade_lazyLoad from './lazyLoad_fade'
